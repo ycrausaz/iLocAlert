6af70a7c2d558d14570ba471f6b85deaec67ddf9
anonymous <anonymous@anonymo.us>
2015-11-19 22:50:22 +0100

    Initial Commit

---
+++ iLocAlert/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iLocAlert
+//
+//  Created by Yann Crausaz on 19/11/15.
+//  Copyright © 2015 Yann Crausaz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
