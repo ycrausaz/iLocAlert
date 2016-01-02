fb2efce666ead2446dc5a138c175a81077160ed8
anonymous <anonymous@anonymo.us>
2016-01-03 00:26:39 +0100

    Added the TableViewControllers and the (accidentally deleted) *.xcdatamodeld file.

---
+++ iLocAlertTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  iLocAlertTableViewController.h
+//  iLocAlert
+//
+//  Created by Yann Crausaz on 02/01/16.
+//  Copyright © 2016 Yann Crausaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iLocAlertTableViewController : UITableViewController
+
+@end
