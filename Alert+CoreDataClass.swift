599fa9f9db408746d5425f440662d5c82ba79625
ycrausaz <yann.crausaz@gmail.com>
2020-08-04 01:23:20 +0200

    Added classes to the model.

---
+++ Alert+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Alert+CoreDataClass.swift
+//  iLocAlert
+//
+//  Created by Yann Crausaz on 04.08.20.
+//  Copyright © 2020 Yann Crausaz. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Alert)
+public class Alert: NSManagedObject {
+
+}
