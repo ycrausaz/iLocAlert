61d61d059f20453cf303b6db2765ae0e4e4cf0b0
ycrausaz <yann.crausaz@gmail.com>
2020-08-03 14:40:27 +0200

    Updated the model.

---
+++ Model/Alert+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Alert+CoreDataClass.swift
+//  iLocAlert
+//
+//  Created by Yann Crausaz on 03.08.20.
+//  Copyright © 2020 Yann Crausaz. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Alert: NSManagedObject {
+
+}
---
+++ Model/Alert+CoreDataProperties.swift
@@ -0,0 +1,34 @@
+//
+//  Alert+CoreDataProperties.swift
+//  iLocAlert
+//
+//  Created by Yann Crausaz on 03.08.20.
+//  Copyright © 2020 Yann Crausaz. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Alert {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Alert> {
+        return NSFetchRequest<Alert>(entityName: "Alert")
+    }
+
+    @NSManaged public var alert_id: Int32
+    @NSManaged public var alert_address: String?
+    @NSManaged public var alert_creation_date: Date?
+    @NSManaged public var alert_email: NSNumber?
+    @NSManaged public var alert_last_modification_date: Date?
+    @NSManaged public var alert_latitude: NSNumber?
+    @NSManaged public var alert_longitude: NSNumber?
+    @NSManaged public var alert_name: String?
+    @NSManaged public var alert_notification: NSNumber?
+    @NSManaged public var alert_radius: NSNumber?
+    @NSManaged public var alert_sms: NSNumber?
+    @NSManaged public var alert_way_entering: NSNumber?
+    @NSManaged public var alert_way_exiting: NSNumber?
+
+}
