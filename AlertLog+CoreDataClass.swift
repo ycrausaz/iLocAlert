8c6f9fb38a10502968fbc60fb096846744ff55fc
ycrausaz <yann.crausaz@gmail.com>
2020-08-04 19:30:44 +0200

    Updated the model with 'AlertLog'.

---
+++ AlertLog+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  AlertLog+CoreDataClass.swift
+//  iLocAlert
+//
+//  Created by Yann Crausaz on 04.08.20.
+//  Copyright © 2020 Yann Crausaz. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(AlertLog)
+public class AlertLog: NSManagedObject {
+
+}
---
+++ AlertLog+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  AlertLog+CoreDataProperties.swift
+//  iLocAlert
+//
+//  Created by Yann Crausaz on 04.08.20.
+//  Copyright © 2020 Yann Crausaz. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension AlertLog {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<AlertLog> {
+        return NSFetchRequest<AlertLog>(entityName: "AlertLog")
+    }
+
+    @NSManaged public var alertlog_id: UUID?
+    @NSManaged public var alert_id: UUID?
+    @NSManaged public var alertlog_datetime: Date?
+    @NSManaged public var alertlog_way_entering: Bool
+    @NSManaged public var alertlog_way_exiting: Bool
+
+}
