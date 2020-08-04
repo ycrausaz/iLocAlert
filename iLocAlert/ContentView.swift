d76efe94990cc4bdcc540934272c004749ab104a
ycrausaz <yann.crausaz@gmail.com>
2020-08-04 20:50:53 +0200

    Trick to ensure that the preview works.

--- iLocAlert/ContentView.swift
+++ iLocAlert/ContentView.swift
@@ -16,6 +16,9 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        return ContentView().environment(\.managedObjectContext, context)
     }
 }
