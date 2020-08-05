91d69591471a00d822332db7e75c9a11b9fe157f
ycrausaz <yann.crausaz@gmail.com>
2020-08-05 17:48:13 +0200

    Added 'AlertDetailMapSetting' and "AlertDetailAlarmSetting'.

---
+++ iLocAlert/AlertDetailAlarmSetting.swift
@@ -0,0 +1,21 @@
+//
+//  AlertDetailAlarmSetting.swift
+//  iLocAlert
+//
+//  Created by Yann Crausaz on 05.08.20.
+//  Copyright © 2020 Yann Crausaz. All rights reserved.
+//
+
+import SwiftUI
+
+struct AlertDetailAlarmSetting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlertDetailAlarmSetting_Previews: PreviewProvider {
+    static var previews: some View {
+        AlertDetailAlarmSetting()
+    }
+}
---
+++ iLocAlert/AlertDetailMapSetting.swift
@@ -0,0 +1,21 @@
+//
+//  AlertDetailMapSetting.swift
+//  iLocAlert
+//
+//  Created by Yann Crausaz on 05.08.20.
+//  Copyright © 2020 Yann Crausaz. All rights reserved.
+//
+
+import SwiftUI
+
+struct AlertDetailMapSetting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlertDetailMapSetting_Previews: PreviewProvider {
+    static var previews: some View {
+        AlertDetailMapSetting()
+    }
+}
